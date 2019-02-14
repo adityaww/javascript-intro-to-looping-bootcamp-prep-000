e7aae5c3ddd4aed35b20070bceaa0396918bbf4b
Aditya Waruwu <aditya.william96@gmail.com>
2019-02-14 03:19:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -22,9 +22,10 @@ function doWhileLoop(num) {
   
   function incrementVariable() {
     i = i + 1;
+    return i;
+  }
     
   do {
   console.log("I run once regardless");
 } while (incrementVariable() <= 0);
-  }
 }
